fece8b0dc8e935affce2aba8be2d7cdb26cb1b08
Liam <ljc3179@gmail.com>
2015-06-04 19:23:50 -0400

    Permanent test class for checking data manipulations

---
+++ test/test.java
@@ -0,0 +1,30 @@
+import java.lang.Math;
+
+/**
+ * Write a description of class teset here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class test
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class teset
+     */
+    public test()
+    {
+        // initialise instance variables
+
+    }
+
+
+
+    public static void main(String args[]){
+        System.out.println(4/3.0 * Math.PI * Math.pow(4,3));
+    }
+
+}
+
