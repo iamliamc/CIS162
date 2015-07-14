63fcacf4cf37e751e0df777bcf294d039e6c3ba5
Liam <ljc3179@gmail.com>
2015-07-14 18:26:57 -0400

    Various project 5 updates

---
+++ project5/Item.java
@@ -0,0 +1,84 @@
+
+/**
+ * Write a description of class Item here.
+ * Item class with 4 methods. 
+ * 
+ * Liam Considine
+ * 7.14.2015
+ */
+public class Item
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private String description;
+    private int weight;
+    private boolean edible;
+    
+    /**
+     * Constructor for objects of class Item
+     */
+    public Item(String name, String description, int weight, boolean edible)
+    {
+        // initialise instance variables
+        this.name = name;
+        this.description = description;
+        this.weight = weight;
+        this.edible = edible;
+    }
+
+    // Set methods
+    public void setName(String name)
+    {
+        // put your code here
+        this.name = name;
+    }
+    
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    
+    public void setWeight(int weight)
+    {
+        this.weight = weight;
+    }
+    
+    public void setEdible(boolean edible)
+    {
+        this.edible = edible;
+    }
+    
+    //Get methods
+    public String getName()
+    {
+        return name;
+    }
+    
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    public int getWeight()
+    {
+        return weight;
+    }
+    
+    public boolean getEdible()
+    {
+        return edible;
+    }
+    
+    public boolean isEdible()
+    {
+        if(edible == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+}
